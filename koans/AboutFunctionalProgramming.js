4f403c932d882619f8ce35bab386303c9d451921
Sathish <sathish316@gmail.com>
2011-08-29 00:02:21 +0530

    spec for Immutability, Recursion

---
+++ koans/AboutFunctionalProgramming.js
@@ -0,0 +1,39 @@
+describe("About Functional programming", function() {
+
+  describe("Variables", function(){
+    it("should be immutable", function(){
+      // Variables are immutable in Functional languages for reasons
+      // that'll be explained. For now use only Vars namespace to
+      // get/set variables to simulate immutability in javascript
+      var oldValue = 42;
+      var newValue = 24;
+      var x = Vars.set('x', oldValue);
+      x = Vars.set('x', newValue);
+
+      expect(Vars.get('x')).toEqual(oldValue);
+    });
+  });
+
+  describe("Iteration", function(){
+    it("should/will not be possible", function(){
+
+    });
+  });
+
+  describe("Recursion", function(){
+    it("should be used to repeatedly do something", function(){
+      var reverse = function (str){
+        if (str.length == 0){
+          return str;
+        } else {
+          return reverse(str.substr(1, str.length)) + str.substr(0, 1);
+        }
+      };
+
+      expect(reverse("RACECAR")).toEqual("RACECAR");
+    });
+  });
+
+
+
+});
