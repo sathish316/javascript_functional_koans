507b58b6a2fd35ee7d113a2c49a96c9732a48846
Sathish <sathish316@gmail.com>
2011-08-29 21:40:00 +0530

    lazy sequences

---
+++ koans/AboutLazyEvaluation.js
@@ -0,0 +1,36 @@
+describe("About Lazy evaluation", function() {
+
+  it("can abstract control structures", function() {
+    function unless(condition, block){
+      if(!condition)
+        block();
+    }
+
+    var answer = 42;
+    unless(true, function(){
+      answer = -42;
+    });
+
+    expect(answer).toBe(42);
+  });
+
+  it("can generate infinite sequences", function(){
+    function lazyFibonacci(){
+      var a = 0;
+      var b = 1;
+      return function(){
+        var next = a;
+        a = b;
+        b = next + b;
+        return next;
+      }
+    }
+    var next = lazyFibonacci();
+    for(var i=1;i<10;i++){
+      next();
+    }
+
+    expect(next()).toEqual(34);
+  });
+
+});
